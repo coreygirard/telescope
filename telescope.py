87da0e28c00766fab14d179dbe30641c4c2628c7
Corey <corey.r.girard@gmail.com>
2018-02-12 21:16:03 -0500

    DSL FTW

--- telescope.py
+++ telescope.py
@@ -1,8 +1,81 @@
 from pprint import pprint
+import sys
+sys.path.append("/Users/coreygirard/Documents/GitHub/foldr")
+import foldr
+
+spine_properties = {'visible':{'()':None},
+                    'bounds':{'()':None},
+                    'ticks':{'.':{'major':{'()':None},
+                                  'minor':{'()':None}}}}
+tree = {'.':{'plot':{'()':None},
+             'scatter':{'()':None},
+             'xlim':{'()':None},
+             'ylim':{'()':None},
+             'line':{'.':{'label':{'()':None}}},
+             'spine':{'.':{'left':{'.':spine_properties},
+                           'right':{'.':spine_properties},
+                           'top':{'.':spine_properties},
+                           'bottom':{'.':spine_properties}}}}}
+
+def convert_tree(tree):
+    if type(tree) == str:
+        if tree.endswith('()'):
+            return {'()':None}
+
+    d = {}
+    for k,v in tree:
+        if k.startswith('.'):
+            if '.' not in d:
+                d['.'] = {}
+
+            k = k[1:]
+            if k.endswith('()'):
+                k = k[:-2]
+                d['.'][k] = {'()':None}
+            else:
+                d['.'][k] = convert_tree(v)
+
+        elif k[0]+k[-1] == '{}':
+            return k
+
+    return d
+
+def merge_tree(tree,ref):
+    d = {}
+    for k,v in tree.items():
+        if v == None:
+            d[k] = v
+        elif type(v) == dict:
+            d[k] = merge_tree(v,ref)
+        elif v[0]+v[-1] == '{}':
+            d[k] = ref[v[1:-1]]
+        else:
+            d[k] = v
+
+    return d
+
+
+def build_tree(filename):
+    with open(filename,'r') as f:
+        data = [line.rstrip() for line in f if line.rstrip() != '']
+
+    data = [(len(line)-len(line.lstrip()),line.lstrip()) for line in data]
+
+    d = {}
+    for k,v in foldr.fromList(data,simple=True):
+        d[k] = v
+
+    for k,v in d.items():
+        d[k] = convert_tree(v)
+    d = merge_tree(d,d)
+    return d
 
 class Telescope(object):
-    def __init__(self,d,callback,path=[]):
-        self.d = d
+    def __init__(self,d,callback,path=[],k=None):
+        if type(d) == str:
+            self.d = build_tree(d)[k]
+        else:
+            self.d = d
         self.callback = callback
         self.path = path
 
@@ -15,14 +88,12 @@ class Telescope(object):
             raise AttributeError('[]'+str(k))
 
         new_path = self.path+[('[]',k)]
-
         if self.d['[]'][k] == None:
             return self.callback(new_path)
         else:
             return Telescope(self.d['[]'][k],
                              self.callback,
                              new_path)
-
 
     def __getattr__(self,k):
         if type(self.d) != dict:
@@ -32,15 +103,17 @@ class Telescope(object):
         if k not in self.d['.'].keys():
             raise AttributeError('.'+str(k))
 
-        return Telescope(self.d['.'][k],
-                         self.callback,
-                         self.path+['.'+str(k)])
+        new_path = self.path+[('.',k)]
+        if self.d['.'][k] == None:
+            return self.callback(new_path)
+        else:
+            return Telescope(self.d['.'][k],
+                             self.callback,
+                             new_path)
 
     def __call__(self,*args,**kwargs):
-        #pprint(self.d,width=15)
-
         if '()' not in self.d.keys():
             raise TypeError('object is not callable')
 
-        new_path = self.path+['()']
+        new_path = self.path+[('()',)]
         return self.callback(new_path,*args,**kwargs)
--- test_telescope.py
+++ test_telescope.py
@@ -13,7 +13,9 @@ tree = {'()':None,
         '[]':{42:{'()':None},
               9:{'()':None}},
         '.':{'abc':{'()':None},
-             'def':{'()':None}}}
+             'ijk':None}}
+
+
 
 #pprint(tree,width=20)
 
@@ -24,18 +26,55 @@ class Flytrap(object):
     def f(self,e):
         self.val = e
 
-class Test(unittest.TestCase):
-    def test_(self):
+class TestBasic(unittest.TestCase):
+    def test_basic(self):
         flytrap = Flytrap()
-        tele = telescope.Telescope(tree,print)
-
+        tele = telescope.Telescope(tree,flytrap.f)
         tele()
+        self.assertEqual(flytrap.val,[('()',)])
+
         tele[42]()
+        self.assertEqual(flytrap.val,[('[]',42), ('()',)])
+
         tele[9]()
-        tele.abc
+        self.assertEqual(flytrap.val,[('[]',9), ('()',)])
+
+        tele.abc()
+        self.assertEqual(flytrap.val,[('.','abc'), ('()',)])
+
+        tele.ijk
+        self.assertEqual(flytrap.val,[('.','ijk')])
 
 
         #self.assertEqual(flytrap.val,['aaa1','bbb1'])
+
+
+spine_properties = {'visible':{'()':None},
+                    'bounds':{'()':None},
+                    'ticks':{'.':{'major':{'()':None},
+                                  'minor':{'()':None}}}}
+tree = {'.':{'plot':'()',
+             'scatter':'()',
+             'xlim':'()',
+             'ylim':'()',
+             'line':{'label':'()'},
+             'spine':{'.':{'left':{'.':spine_properties},
+                           'right':{'.':spine_properties},
+                           'top':{'.':spine_properties},
+                           'bottom':{'.':spine_properties}}}}}
+
+class TestBasic(unittest.TestCase):
+    def test_basic(self):
+        flytrap = Flytrap()
+        tele = telescope.Telescope('/Users/coreygirard/Documents/GitHub/telescope/chart.yml',print,k='chart')
+        tele.spine.left.visible('aaa','bbb')
+        tele.plot()
+        tele.line.label()
+        tele.spine.left.visible()
+        #self.assertEqual(flytrap.val,[('()',)])
+
+
+
 
 
 def load_tests(loader, tests, ignore):
